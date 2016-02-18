1ffea43abd9556607e9ddf1aaa88ac2d70f0066c
James O'Neal & Mason Pierce <pair+jamesponeal.masopie@devbootcamp.com>
2016-02-18 14:24:10 -0600

    ajaxify answer-a-question link

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,15 +1,32 @@
 get '/questions/:id/answers' do
   @question_id = params[:id]
-  erb :'answers/new'
+  if request.xhr?
+    erb :'answers/new', :layout => false
+  else
+    erb :'answers/new'
+  end
 end
 
 post '/questions/:id/answers' do
   @answer = Answer.new(body: params[:body], user_id: current_user.id, question_id: params[:id])
-  if @answer.save
-    redirect "/questions/#{params[:id]}"
+  p "*************************"
+  p @answer.body
+  if request.xhr?
+    if @answer.save
+      status 200
+      @question = Question.find(params[:id])
+      p @question.id
+      erb :"answers/_new", :layout => false
+    else
+      status 422
+    end
   else
-    @question_id = params[:id]
-    @errors = @answer.errors.full_messages
-    erb :'answers/new'
+    if @answer.save
+      redirect "/questions/#{params[:id]}"
+    else
+      @question_id = params[:id]
+      @errors = @answer.errors.full_messages
+      erb :'answers/new'
+    end
   end
 end
